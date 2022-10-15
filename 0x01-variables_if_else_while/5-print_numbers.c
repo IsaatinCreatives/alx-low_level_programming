e923999d9a4c792d268ffbec4e9b972dd691cc39
Akintunde Taiwo <aitaiwo01@gmail.com>
2022-10-15 13:29:58 -0700

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase.
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
  * Return: 0
  */
 int main(void)
 {
-	int numn;
+	int num;
 
 	for (num = 0; num < 10; num++)
 		printf("%d", num);
