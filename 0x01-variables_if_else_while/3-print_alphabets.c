5520926898bdf594afd4353fc3106051680603c5
Akintunde Taiwo <aitaiwo01@gmail.com>
2022-10-14 21:04:04 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,8 +12,10 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
-	for putchar(letter = 'A'; letter <= 'Z'; letter++);
+	for (letter = 'A'; letter <= 'Z'; letter++);
 		putchar(letter);
+
+	putchar('\n');
 
 	return (0);
 }
