7f8726ca78e58daaf734568f54bf3600d8bc10d4
Akintunde Taiwo <aitaiwo01@gmail.com>
2022-11-08 18:55:38 -0800

    A function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,8 +8,7 @@
  * Return: Pointer to concatenated string (SUCCESS) or
  * NULL if @ac == 0 or @av == NULL (FAILURE) or
  * NULL if if insufficient memory was available (FAILURE)
- *
- * */
+ */
 
 char *argstostr(int ac, char **av)
 {
