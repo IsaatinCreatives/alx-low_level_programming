276adc6541ecbb5c99c563babf1b83b01ae8ba22
Akintunde Taiwo <aitaiwo01@gmail.com>
2022-11-17 14:52:48 -0800

    A function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -21,100 +21,100 @@ void print_all(const char * const format, ...)
 	format_struct_ptr fmt_arr[] = {
 		{'c', print_char},
 		{{'f', print_float},
-			{'s', print_string},
-			{'\0', NULL}
-		};
-		void (*get_func_ptr)(va_list *);
-		va_list args;
+		{'s', print_string},
+		{'\0', NULL}
+	};
+	void (*get_func_ptr)(va_list *);
+	va_list args;
 
-		va_start(args, format);
+	va_start(args, format);
 
-		while (format && format[j] != '\0')
+	while (format && format[j] != '\0')
+	{
+		get_func_ptr = get_func(format[j], fmt_arr);
+
+		if (get_func_ptr)
 		{
-			get_func_ptr = get_func(format[j], fmt_arr);
-
-			if (get_func_ptr)
-			{
-				printf("%s", separator);
-				get_func_ptr(&args);
-				separator = ", ";
-			}
-
-			j++;
+			printf("%s", separator);
+			get_func_ptr(&args);
+			separator = ", ";
 		}
 
-		va_end(args);
-		printf("\n");
+		j++;
 	}
 
-	/**
-	 * get_func - gets corresponding function of format type
-	 * @fmt_arr: format types array
-	 * @identifier: format type
-	 *
-	 * Return: pointer to function (SUCCESS) or
-	 * NULL (FAILURE)
-	 */
+	va_end(args);
+	printf("\n");
+}
 
-	void (*get_func(char identifier, struct format_struct *fmt_arr))(va_list *)
+/**
+ * get_func - gets corresponding function of format type
+ * @fmt_arr: format types array
+ * @identifier: format type
+ *
+ * Return: pointer to function (SUCCESS) or
+ * NULL (FAILURE)
+ */
+
+void (*get_func(char identifier, struct format_struct *fmt_arr))(va_list *)
+{
+	int i = 0;
+
+	while (fmt_arr[i].format)
 	{
-		int i = 0;
-
-		while (fmt_arr[i].format)
+		if (fmt_arr[i].format == identifier)
 		{
-			if (fmt_arr[i].format == identifier)
-			{
-				return (fmt_arr[i].fmt_print_func);
-			}
-
-			i++;
+			return (fmt_arr[i].fmt_print_func);
 		}
 
-		return (fmt_arr[i].fmt_print_func);
+		i++;
 	}
 
-	/**
-	 * print_char - prints a char
-	 * @arg: pointer to a char
-	 */
+	return (fmt_arr[i].fmt_print_func);
+}
 
-	void print_char(va_list *arg)
-	{
-		printf("%c", va_arg(*arg, int));
-	}
+/**
+ * print_char - prints a char
+ * @arg: pointer to a char
+ */
 
-	/**
-	 * print_int - prints an int
-	 * @arg: pointer to int
-	 */
+void print_char(va_list *arg)
+{
+	printf("%c", va_arg(*arg, int));
+}
 
-	void print_int(va_list *arg)
-	{
-		printf("%d", va_arg(*arg, int));
-	}
+/**
+ * print_int - prints an int
+ * @arg: pointer to int
+ */
 
-	/**
-	 * print_float - prints a float data type
-	 * @arg: pointer to float
-	 */
+void print_int(va_list *arg)
+{
+	printf("%d", va_arg(*arg, int));
+}
 
-	void print_float(va_list *arg)
-	{
-		printf("%f", va_arg(*arg, double));
-	}
+/**
+ * print_float - prints a float data type
+ * @arg: pointer to float
+ */
 
-	/**
-	 * print_string - prints a string
-	 * @arg: pointer to string
-	 */
+void print_float(va_list *arg)
+{
+	printf("%f", va_arg(*arg, double));
+}
 
-	void print_string(va_list *arg)
-	{
-		char *str[2];
-		int i;
+/**
+ * print_string - prints a string
+ * @arg: pointer to string
+ */
 
-		str[0] = va_arg(*arg, char *);
-		str[1] = "(nil)";
-		i = str[0] == NULL;
-		printf("%s", str[i]);
-	}
+void print_string(va_list *arg)
+{
+	char *str[2];
+	int i;
+
+	str[0] = va_arg(*arg, char *);
+	str[1] = "(nil)";
+	i = str[0] == NULL;
+	printf("%s", str[i]);
+}
