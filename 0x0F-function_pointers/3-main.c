d12e5e27b332a96379c94198907d4a42cd20315c
Akintunde Taiwo <aitaiwo01@gmail.com>
2022-11-17 13:45:48 -0800

    3-main.c created

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,40 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "3-calc.h"
 
 /**
- * main - prints the result between two operators.
- * @argc: number of arguments received.
- * @argv: array of arguments received.
+ * main - Entry point
+ * @argc: length of command line arguments
+ * @argv: double pointer to cli arguments
  *
- * Return: 0 (success).
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	int num1 = 0;
-	int num2 = 0;
-	int res;
+	int (*func_ptr)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 
-	res = get_op_func(argv[2])(num1, num2);
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-	printf("%d\n", res);
+	func_ptr = get_op_func(argv[2]);
+
+	if (func_ptr == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 }
